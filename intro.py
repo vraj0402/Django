074c49029f42486f5a80e55ee526432b536f3351
vraj0402 <65901726+vraj0402@users.noreply.github.com>
2021-05-27 13:24:28 +0530

    basics of python
    simple codes of python

---
+++ intro.py
@@ -0,0 +1,31 @@
+print("hello world")
+
+'''this is multi line comment'''
+"""this is also multiline comment"""
+#this is single line comment
+
+a=10;
+b=10.5
+c="oxygen"
+# or can be declared as a,b,c=10,10.5,"oxygen"
+
+print(a)
+print("the value of b is: ",b)
+print("written by: ",c)
+
+a1=b1=c1=10
+print(a1,b1,c1)
+print("value of b1:",b1)
+print("value of c1:",c1)
+
+
+n1=10
+n2=10.1
+print(n1," is type of ",type(n1))
+print(n2," is type of ",type(n2))
+
+print(n2,"is int type? ",isinstance(n2,int))
+
+n3= 1 + 2j
+print(n3+5j)
+print(type(n3))
