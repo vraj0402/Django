357fd5cbe6c41330de7b60ab0455943590e41d29
vraj0402 <vraj9924431762@gmail.com>
2021-06-09 19:57:30 +0530

    Update settings.py

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+2khjh6!5)w+ffa!u-c5&2+#)fi)aek_e*i*i@p!36xzi0-y8j'
+SECRET_KEY = config("SECRET_KEY")   
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
